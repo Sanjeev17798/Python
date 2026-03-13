e820019ddb11f39d34e05c23f6e8889349e91d30
Sanjeev <Sanjeevyadav17798@gmail.com>
2026-03-13 21:51:46 +0530

    largest file

---
+++ find_largest_file.py
@@ -0,0 +1,17 @@
+# This will find the largest filest into current directory 
+import os
+
+largest_file = ""
+largest_size = 0
+
+for file in os.listdir():
+    if os.path.isfile(file):
+        size = os.path.getsize(file)
+        if size > largest_size:
+            largest_size = size
+            largest_file = file
+
+print("Largest file:", largest_file)
+print("Size:", largest_size, "bytes")
+
+# agar user sy input dena hai like need to puth path we can just put as on top path=input("")
